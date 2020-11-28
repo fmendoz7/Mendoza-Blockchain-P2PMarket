ea9018997c47e9063a02f71d4691c8da202be5cf
fmendoz7 <fmendoz7@asu.edu>
2020-11-28 09:28:19 -0700

    Added functions for store smart contract

--- contracts/Store.sol
+++ contracts/Store.sol
@@ -21,7 +21,7 @@ contract Store is Ownable {
         uint quantity;
         bool exists;
     }
-
+    /*------------------------------------------------------------------------------------------------------------------*/
     //EVENTS
     event AddedItem(uint indexed id, string name, uint price, uint quantity);
     event RemovedItem(uint indexed id);
@@ -29,7 +29,7 @@ contract Store is Ownable {
     event BoughtItem(uint indexed id, address indexed buyer, uint quantity);
     event DeletedStore(address indexed addr, address indexed owner);
     event WithdrewFunds(address indexed addr, uint amount);
-
+    /*------------------------------------------------------------------------------------------------------------------*/
     //MODIFIERS
     // If item does not exist
     modifier itemExists(uint _id) {
@@ -44,7 +44,7 @@ contract Store is Ownable {
         require(paused == false, "ERROR: Marketplace is PAUSED");
         _;
     }
-
+    /*------------------------------------------------------------------------------------------------------------------*/
     //CONSTRUCTOR
         //NOTE: Be sure to use memory for string parameters!
     constructor(address _owner, address _marketplaceAddr, string memory _name) public {
@@ -54,5 +54,53 @@ contract Store is Ownable {
         name = _name;
         itemIterator = 0;
     }
-
+    /*------------------------------------------------------------------------------------------------------------------*/
+    //PURPOSE: Get properties of the store as a tuple
+    function getValues() public view returns (address, address, string memory, uint) {
+        return (owner(), marketplaceAddress, name, itemIterator);
+    }
+    /*------------------------------------------------------------------------------------------------------------------*/
+    //PURPOSE: Add item to store inventory
+    function addItem(string memory _name, uint _price, uint _quantity) public onlyOwner isNotPaused {
+        emit AddedItem(itemIterator, _name, _price, _quantity);
+        items[itemIterator] = Item(itemIterator, _name, _price, _quantity, true);
+        itemIterator = SafeMath.add(itemIterator, 1);
+    }
+    /*------------------------------------------------------------------------------------------------------------------*/
+    //PURPOSE: Remove item from store inventory
+    function removeItem(uint _id) public itemExists(_id) onlyOwner isNotPaused {
+        emit RemovedItem(_id);
+        delete items[_id];
+    }
+    /*------------------------------------------------------------------------------------------------------------------*/
+    //PURPOSE: Change price of an item
+    function changePrice(uint _id, uint _price) public itemExists(_id) onlyOwner isNotPaused {
+        emit ChangedPrice(_id, items[_id].price, _price);
+        items[_id].price = _price;
+    }
+    /*------------------------------------------------------------------------------------------------------------------*/
+    //PURPOSE: Purchase item
+    function buyItem(uint _id, uint _quantity) public payable itemExists(_id) isNotPaused {
+        require(items[_id].price <= msg.value, "ERROR: Insufficient Funds.");
+        require(items[_id].quantity >= _quantity, "ERROR: Insufficient Stock Of Item.");
+        emit BoughtItem(_id, msg.sender, _quantity);
+        items[_id].quantity = items[_id].quantity - _quantity;
+    }
+    /*------------------------------------------------------------------------------------------------------------------*/
+    //PURPOSE: Delete Store Instance
+    function deleteStore() external isNotPaused {
+        // Verify that call is made from the marketplace contract
+        require(msg.sender == marketplaceAddress, "ERROR: You do NOT have permission to perform this action.");
+        emit DeletedStore(address(this), owner());
+        // cast to address(uint160) to make address payable
+        selfdestruct(address(uint160(owner())));
+    }
+    /*------------------------------------------------------------------------------------------------------------------*/
+    //PURPOSE: Withdraw funds
+    function withdrawFunds(uint _amount) public onlyOwner {
+        require(_amount <= address(this).balance, "ERROR: Withdrawal amount exceeds available balance.");
+        emit WithdrewFunds(owner(), _amount);
+        // cast to address(uint160) to make address payable
+        address(uint160(owner())).transfer(_amount);
+    }
 }
