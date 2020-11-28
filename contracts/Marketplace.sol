25c1fc12e46c5c5df496a311a2c2ba382816b8fe
fmendoz7 <fmendoz7@asu.edu>
2020-11-28 09:07:58 -0700

    Added mappings and events for marketplace smart contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -5,5 +5,17 @@ import "@openzeppelin/contracts/lifecycle/Pausable.sol";
 
 //Utilize Ownable and Pausable libraries from openzeppelin
 contract Marketplace is Ownable, Pauasable {
-    
+
+    //Address mappings of admins, storeOwners, stores
+    mapping(address => bool) public admins;
+    mapping(address => bool) public storeOwners;
+    mapping(address => Store[]) public stores;
+
+    //Events to add/revoke various privileges, add/delete stores
+    event AddedAdmin(address indexed addr);
+    event RemovedAdmin(address indexed addr);
+    event AddedStoreOwner(address indexed addr);
+    event RemovedStoreOwner(address indexed addr);
+    event AddedStore(address indexed owner, string name);
+    event DeletedStore(address indexed addr, address indexed owner);
 }
