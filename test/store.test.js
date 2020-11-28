c8ee10be21677641570c96197ec1026b885c1b1f
fmendoz7 <fmendoz7@asu.edu>
2020-11-28 08:55:26 -0700

    Added TEST #2 for store.test.js

--- test/store.test.js
+++ test/store.test.js
@@ -18,6 +18,7 @@ contract('Store', (accounts) => {
   const itemPrice = web3.toWei(1, 'ether');
   const itemQty = 7;
 
+ /*-----------------------------------------------------------------------------------------------------------------------*/
   /**
    * Test that a store owner is able to both open and close a store.
    */
@@ -34,5 +35,27 @@ contract('Store', (accounts) => {
     assert.isTrue(stores.length > 0, 'store added');
     assert.isTrue(deletedStore.length === 0, 'store removed');
   })
+  /*-----------------------------------------------------------------------------------------------------------------------*/
+  /**
+   * Test that a store owner is able to add and remove an item from the store.
+   */
+  it('TEST #2: Add and remove item from store', async () => {
+    const instance = await Marketplace.deployed();
+    await instance.addStoreOwner(alice);
+    await instance.addStore(storeName, { from: alice });
+
+    const stores = await instance.getStores(alice);
+    const storeInstance = await Store.at(stores[0]);
+
+    await storeInstance.addItem(itemName, itemPrice, itemQty, { from: alice, to: stores[0] });
+    const itemAdded = await storeInstance.items(0);
+
+    await storeInstance.removeItem(0, { from: alice, to: stores[0] })
+    const itemRemoved = await storeInstance.items(0);
+
+    assert.isTrue(itemAdded[1] === itemName, 'potion was added');
+    assert.isTrue(itemRemoved[1] === '', 'potion was removed')
+  })
+  /*-----------------------------------------------------------------------------------------------------------------------*/
 
 })
