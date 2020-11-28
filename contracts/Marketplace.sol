46d1e250a4b6bbefc620b26fb275c5378a5b706d
fmendoz7 <fmendoz7@asu.edu>
2020-11-28 09:10:58 -0700

    Added modifiers for marketplace smart contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -18,4 +18,29 @@ contract Marketplace is Ownable, Pauasable {
     event RemovedStoreOwner(address indexed addr);
     event AddedStore(address indexed owner, string name);
     event DeletedStore(address indexed addr, address indexed owner);
+
+    // Scenario: Non-admin user makes call
+    modifier onlyAdmin() {
+        require(admins[msg.sender] == true, "ERROR: You do not have permission to perform this action.");
+        _;
+    }
+
+    // Scenario: Non-owner user makes call
+    modifier onlyStoreOwner() {
+        require(storeOwners[msg.sender] == true, "ERROR: You are NOT a store owner.");
+        _;
+    }
+
+    // Scenario: User that doesn't own specific store makes call
+    modifier validStoreOwner() {
+        bool exists = false;
+        Store[] memory storeList = stores[msg.sender];
+        for (uint i = 0; i < storeList.length; i++) {
+            if (storeList[i].owner() == msg.sender) {
+                exists = true;
+            }
+        }
+        require(exists, "ERROR: You are NOT the DESIGNATED store owner.");
+        _;
+    }
 }
