d527662fb97096549bd7c3347c215bd7c97e9086
fmendoz7 <fmendoz7@asu.edu>
2020-11-28 09:06:49 -0700

    Added @openzeppelin dependencies for marketplace smart contract

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -1,1 +1,9 @@
 pragma solidity ^0.5.0;
+
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/lifecycle/Pausable.sol";
+
+//Utilize Ownable and Pausable libraries from openzeppelin
+contract Marketplace is Ownable, Pauasable {
+    
+}
