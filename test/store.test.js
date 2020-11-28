0546ec2ba4ac263d49990311637abcf240d5c1d9
fmendoz7 <fmendoz7@asu.edu>
2020-11-28 08:59:35 -0700

    Added TEST #5 for store.test.js. Will add comments later

--- test/store.test.js
+++ test/store.test.js
@@ -6,22 +6,20 @@ const Web3 = require('web3');
 web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 contract('Store', (accounts) => {
-  // NOTE: These accounts start from from the second index. accounts[0] will deploy the store
+  // NOTE: These accounts start from from the second index
+  // NOTE: accounts[0] will deploy the store
   const alice = accounts[1];
   const bob = accounts[2];
   const charlie = accounts[3];
   const diana = accounts[4];
 
   // Store values
-  const storeName = 'Potion Seller';
-  const itemName = 'Potion';
+  const storeName = 'Foodstuff Merchant';
+  const itemName = 'Turkey';
   const itemPrice = web3.toWei(1, 'ether');
   const itemQty = 7;
 
  /*-----------------------------------------------------------------------------------------------------------------------*/
-  /**
-   * Test that a store owner is able to both open and close a store.
-   */
   it('TEST #1: Can instantiate and delete a store', async () => {
     const instance = await Marketplace.deployed();
     await instance.addStoreOwner(alice);
@@ -36,9 +34,6 @@ contract('Store', (accounts) => {
     assert.isTrue(deletedStore.length === 0, 'store removed');
   })
   /*-----------------------------------------------------------------------------------------------------------------------*/
-  /**
-   * Test that a store owner is able to add and remove an item from the store.
-   */
   it('TEST #2: Add and remove item from store', async () => {
     const instance = await Marketplace.deployed();
     await instance.addStoreOwner(alice);
@@ -53,13 +48,10 @@ contract('Store', (accounts) => {
     await storeInstance.removeItem(0, { from: alice, to: stores[0] })
     const itemRemoved = await storeInstance.items(0);
 
-    assert.isTrue(itemAdded[1] === itemName, 'potion was added');
-    assert.isTrue(itemRemoved[1] === '', 'potion was removed')
+    assert.isTrue(itemAdded[1] === itemName, 'turkey was added');
+    assert.isTrue(itemRemoved[1] === '', 'turkey was removed')
   })
   /*-----------------------------------------------------------------------------------------------------------------------*/
-  /**
-   * Test that a user can change the price of an item.
-   */
   it('TEST #3: Change item price successfully', async () => {
     const priceChange = web3.toWei(42, 'ether');
     const instance = await Marketplace.deployed();
@@ -78,9 +70,6 @@ contract('Store', (accounts) => {
     assert.isTrue(priceInEther === '42', 'changed price')
   })
   /*-----------------------------------------------------------------------------------------------------------------------*/
-  /**
-   * Test that a user can successfully purchase an item. Validate by checking quantity of item after purchase.
-   */
   it('TEST #4: Purchase item successfully', async () => {
     const qtyBuying = 3;
     const instance = await Marketplace.deployed();
@@ -95,6 +84,28 @@ contract('Store', (accounts) => {
     const item = await storeInstance.items(0);
 
     assert.isTrue(item[3].toNumber() === itemQty - qtyBuying, 'bought item successfully');
+  })
+/*-----------------------------------------------------------------------------------------------------------------------*/
+  it('TEST #5: Withdraw funds successfully', async () => {
+    const qtyBuying = 3;
+    const instance = await Marketplace.deployed();
+    await instance.addStoreOwner(diana);
+    await instance.addStore(storeName, { from: diana });
+
+    const stores = await instance.getStores(diana);
+    const storeInstance = await Store.at(stores[0]);
+    await storeInstance.addItem(itemName, itemPrice, itemQty, { from: diana, to: stores[0] });
+    await storeInstance.buyItem(0, qtyBuying, { from: bob, to: stores[0], value: qtyBuying * itemPrice });
+
+    const dianaBalance = web3.eth.getBalance(diana);
+    const storeBalance = web3.eth.getBalance(stores[0]);
+    await storeInstance.withdrawFunds(storeBalance, { from: diana, to: stores[0] });
+    const dianaBalanceAfterWithdrawal = web3.eth.getBalance(diana);
+
+    const beforeWithdrawal = web3.fromWei(dianaBalance, 'ether');
+    const afterWithrawal = web3.fromWei(dianaBalanceAfterWithdrawal, 'ether')
+
+    assert.isTrue(beforeWithdrawal.lt(afterWithrawal), 'diana withdrew successfully');
   })
   
 })
